04fe4314b243188c0db164fdd8820fd57bd485dc
Adam <adammiller.2000nj@gmail.com>
2023-09-19 00:52:02 -0400

    added panel flip on correct answer

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ function answerSlot(count) {
     for(i = 0; i < count; i++){
         const board = document.querySelector('.game-board');
         let slot = document.createElement('div');
-        board.appendChild(slot).className = 'answer';
-        let logo = document.createElement('img');
+        board.appendChild(slot).className = 'panel';
         let answer = document.createElement('p');
-        slot.appendChild(logo);
-        slot.appendChild(answer);
+        let points = document.createElement('p');
+        slot.appendChild(answer).className = 'answer vanish';
+        slot.appendChild(points).className = 'points vanish';
         slot.id = `slot${i + 1}`;
-        answer.id = `answer${i + 1}`;
-        answer.innerText = 'butts';
+        answer.id = `panel${i + 1}`;
+        answer.class = 'panelText'
+        answer.innerText = 'balls';
+        points.innerText = '74'
     }
 }
 answerSlot(8);
@@ -17,51 +19,41 @@ let answerInput = document.getElementById('answer-input');
 let submitBtn = document.getElementById('submit-button');
 submitBtn.addEventListener('click', () => {
     switch(answerInput.value){
-        case document.getElementById('answer1').innerText:
+        case document.getElementById('panel1').innerText:
             console.log('WORKED 1');
             slot1.classList.add('flip')
+            document.querySelectorAll('.flip > p').forEach(el => el.classList.remove('vanish'));
             break;
-        case document.getElementById('answer2').innerText:
+        case document.getElementById('panel2').innerText:
             console.log('WORKED 2')
             slot2.classList.add('flip')
             break;
-        case document.getElementById('answer3').innerText:
+        case document.getElementById('panel3').innerText:
             console.log('WORKED 3')
             slot3.classList.add('flip')
             break;
-        case document.getElementById('answer4').innerText:
+        case document.getElementById('panel4').innerText:
             console.log('WORKED 4')
             slot4.classList.add('flip')
             break;
-        case document.getElementById('answer5').innerText:
+        case document.getElementById('panel5').innerText:
             console.log('WORKED 5')
             slot5.classList.add('flip')
             break;
-        case document.getElementById('answer6').innerText:
+        case document.getElementById('panel6').innerText:
             console.log('WORKED 6')
             slot6.classList.add('flip')
             break;
-        case document.getElementById('answer7').innerText:
+        case document.getElementById('panel7').innerText:
             console.log('WORKED 7')
             slot7.classList.add('flip')
             break;
-        case document.getElementById('answer8').innerText:
+        case document.getElementById('panel8').innerText:
             console.log('WORKED 8')
             slot8.classList.add('flip')
             break;
         default:
             console.log('ERROR')
     }
-})
-
-let answers = document.querySelectorAll('.answer');
-answers.forEach(el => {
-    el.addEventListener('click', () => {
-        if(el.classList.contains('balls')){
-            console.log('true')
-        } else {
-            console.log('false')
-        }
-    })
-})
-
+    answerInput.value = '';
+});
